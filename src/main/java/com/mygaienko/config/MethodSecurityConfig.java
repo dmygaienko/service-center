4def2cfe4855880441f8b5043f62320b487b3aa0
dmygaenko <dmygaenko@gmail.com>
2016-11-14 13:58:21 +0200

    test getReference

--- src/main/java/com/mygaienko/config/MethodSecurityConfig.java
+++ src/main/java/com/mygaienko/config/MethodSecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.access.expression.method.MethodSecurityExpre
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
+
+/* http://stackoverflow.com/questions/6632982/how-to-create-custom-methods-for-use-in-spring-security-expression-language-anno */
 /**
  * Created by enda1n on 12.11.2016.
  */
