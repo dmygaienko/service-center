976b28df28d7bb8b6d30d27eb13a62ba7ee98b18
dmygaenko <dmygaenko@gmail.com>
2016-11-15 12:53:45 +0200

    test getReference

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,5 @@
+
+ --<user_authorities user_id="1" authorities="ROLE_CLIENT"/>
+
+ -- <user id="1" email="email@test.com" first_name="FirstName" surname="SurName" address="Address" password="qwerty1234"
+ --          contact_number="ContactNumber"/>
---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,10 @@
+create table user_authorities
+{
+user_id int NOT NULL PRIMARY KEY,
+authorities varchar(255)
+}
+
+ --<user_authorities user_id="1" authorities="ROLE_CLIENT"/>
+
+ -- <user id="1" email="email@test.com" first_name="FirstName" surname="SurName" address="Address" password="qwerty1234"
+ --          contact_number="ContactNumber"/>
