ee86664d7ec8449517329c6c68d9200d591b35dd
dmygaenko <dmygaenko@gmail.com>
2016-12-08 14:23:16 +0200

    add test

--- src/test/java/com/mygaienko/service/TestUtils.java
+++ src/test/java/com/mygaienko/service/TestUtils.java
@@ -11,6 +11,19 @@ public class TestUtils {
     @Test
     public void testEncoder() throws Exception {
         System.out.println(new BCryptPasswordEncoder(16).encode("password"));
+    }
 
+    @Test
+    public void testAction() {
+        test(() -> System.out.println("ololo"));
+    }
+
+    public static void test(Action action){
+        action.doAction();
+    }
+
+    @FunctionalInterface
+    interface Action {
+        void doAction();
     }
 }
