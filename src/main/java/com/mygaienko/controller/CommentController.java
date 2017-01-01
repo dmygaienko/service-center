a93711e5dde8b65570e1d9f56d4d06bb148b475e
dmygaien <dmygaenko@gmail.com>
2017-01-01 16:05:14 +0200

    added images and added modal dialog

---
+++ src/main/java/com/mygaienko/controller/CommentController.java
@@ -0,0 +1,10 @@
+package com.mygaienko.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by enda1n on 01.01.2017.
+ */
+@Controller
+public class CommentController {
+}
---
+++ src/main/java/com/mygaienko/service/ProductService.java
@@ -0,0 +1,10 @@
+package com.mygaienko.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by enda1n on 01.01.2017.
+ */
+@Service
+public class ProductService {
+}
